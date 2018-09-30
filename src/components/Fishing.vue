2e47636142d9afe2dd88f3b531d58fb9acf40b09
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-09-30 16:30:02 +0800

    build

--- src/components/Fishing.vue
+++ src/components/Fishing.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
     <div class="ui grid">
-      <div class="three wide column" v-for = "(f, index) in fishs1" :key="f" v-bind:class="face0 == index ? 'face' : 'back'" @click = "flip(index,0)">
+      <div class="three wide column" v-for = "(f, index) in fishs1" :key="f" v-bind:class="[face0 == index ? 'face' : 'back', f ? 'good' : 'null']" @click = "flip(index,0)">
         <img v-bind:class="fishs1[face0] == fishs2[face1] ? 'gold' : '' " :src="f"/>
+        <span class="big" v-show="!f">x</span>
       </div>
     </div>
     <div class="ui grid">
-      <div class="three wide r column" v-for = "(f, index) in fishs2" :key="index" v-bind:class="face1 == index ? 'face' : 'back'" @click = "flip(index,1)">
+      <div class="three wide r column" v-for = "(f, index) in fishs2" :key="index" v-bind:class="[face1 == index ? 'face' : 'back', f ? 'good' : 'null']" @click = "flip(index,1)">
         <img v-bind:class="fishs1[face0] == fishs2[face1] ? 'gold' : ''" :src="f"/>
+        <span class="big" v-show="!f">x</span>
       </div>
     </div>
   </div>
@@ -41,11 +43,20 @@ export default {
         }
       }
     },
+    isWin: function () {
+      var ww = true
+      for (var i = 0; i < this.fishs1.length; i++) {
+        if (this.fishs1[i] !== '') { ww = false }
+      }
+      return ww
+    },
     remove: function () {
-      this.fishs1.splice(this.face0, 1)
-      this.fishs2.splice(this.face1, 1)
+      this.fishs1[this.face0] = ''
+      this.fishs2[this.face1] = ''
+      // this.fishs1.splice(this.face0, 1)
+      // this.fishs2.splice(this.face1, 1)
       this.flipback()
-      if (!this.fishs1.length && !this.fishs2.length) {
+      if (this.isWin()) {
         this.win()
         this.reset()
       }
@@ -83,7 +94,11 @@ export default {
   img.gold {
     border: 5px gold ridge;
   }
+  .grid {
+    padding: 15px;
+  }
   .column {
+    min-height: 20vh;
     margin: 10px;
     border: 3px black solid;
     border-radius: 15px;
@@ -97,11 +112,17 @@ export default {
   .column.back.r {
     background-color: #faa;
   }
+  .column.null {
+    visibility: hidden !important;
+  }
   a {
     cursor: pointer;
     font-size: 2em;
   }
   .ui.bottom.attached {
     top: -5px;
+  }
+  .big {
+    font-size: 5em;
   }
 </style>
