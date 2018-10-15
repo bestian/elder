71a9d8256761bf75e832863e84cdef601745e8f2
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-10-15 08:16:14 +0800

    Update src/main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLocalStorage from 'vue-localstorage'
+import Ads from 'vue-google-adsense'
+
+Vue.use(require('vue-script2'))
+
+Vue.use(Ads.Adsense)
+Vue.use(Ads.InArticleAdsense)
+Vue.use(Ads.InFeedAdsense)
 
 Vue.config.productionTip = false
+Vue.use(VueLocalStorage)
 
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  components: { App },
+  template: '<App/>'
 })
