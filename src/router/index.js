5122c364f80fa39ab83930b69c5c1eb9d436477c
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-10-15 08:05:40 +0800

    small

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,59 @@
 import Vue from 'vue'
+import Tap from 'vtap'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Main from '@/components/Main'
+import Intro from '@/components/Intro'
+import Pair from '@/components/Pair'
+import Spin from '@/components/Spin'
+import Name from '@/components/Name'
+import Fishing from '@/components/Fishing'
+import History from '@/components/History'
+import Editor from '@/components/Editor'
 
 Vue.use(Router)
+Vue.use(Tap)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Main',
+      component: Main
+    },
+    {
+      path: '/intro',
+      name: 'Intro',
+      component: Intro
+    },
+    {
+      path: '/pair',
+      name: 'Pair',
+      component: Pair
+    },
+    {
+      path: '/spin',
+      name: 'Spin',
+      component: Spin
+    },
+    {
+      path: '/name',
+      name: 'Name',
+      component: Name
+    },
+    {
+      path: '/fishing',
+      name: 'Fishing',
+      component: Fishing
+    },
+    {
+      path: '/history',
+      name: 'History',
+      component: History
+    },
+    {
+      path: '/edit',
+      name: 'Editor',
+      component: Editor
     }
   ]
 })
