435221bd1f474b227e4f90e1b0c725040cba563e
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-10-24 06:35:11 +0800

    add playok

--- src/components/Outer.vue
+++ src/components/Outer.vue
@@ -7,13 +7,13 @@
           {{idx+1}}. {{o.n}}
         </h1>
         <p>
-          <iframe width="100%" height="315" :src="o.tube" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+          <iframe v-if="o.tube" width="100%" height="315" :src="o.tube" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </p>
         <p class="description">{{o.d}}</p>
         <div class="ui three massive buttons">
           <a class="ui red button" :href = "o.pc" target="_blank"><i class =" desktop icon"/><br/>電腦版</a>
-          <a class="ui green button" :href = "o.ios" target="_blank"><i class ="app store icon" /><br/>App Store</a>
-          <a class="ui blue button" :href = "o.and" target="_blank"><i class = "google play icon" /><br/>Google Play</a>
+          <a class="ui green button" :href = "o.ios || o.pc" target="_blank"><i v-if="o.ios" class ="app store icon" /><i v-else class =" desktop icon"/><br/><span>Apple行動版</span></a>
+          <a class="ui blue button" :href = "o.and || o.pc" target="_blank"><i v-if="o.ios" class = "google play icon" /><i v-else class =" desktop icon"/><br/><span>Andorid行動版</span></a>
         </div>
       </div>
     </div>
@@ -40,6 +40,11 @@ export default {
           pc: 'https://world-of-goo.en.softonic.com/',
           ios: 'https://itunes.apple.com/cn/app/world-of-goo/id401301276?mt=8',
           and: 'https://play.google.com/store/apps/details?id=com.twodboy.worldofgoofull&hl=en_US'
+        },
+        { n: '網路下棋',
+          d: '長輩年輕時喜歡下棋嗎?在線遊戲-和其他真人玩家對弈!有象棋, 圍棋, 五子棋, 西洋棋, 黑白棋等',
+          tube: 'https://www.youtube.com/embed/duXjgFYkHpM',
+          pc: 'https://www.playok.com/'
         }
       ]
     }
